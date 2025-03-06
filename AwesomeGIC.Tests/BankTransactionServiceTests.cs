393a591f435884d9cfff0316ea4b9f9f6f84a3c8
Jerwin Aggabao <jerwindev@outlook.com>
2025-03-07 01:36:55 +0800

    added test project

---
+++ AwesomeGIC.Tests/BankTransactionServiceTests.cs
@@ -0,0 +1,189 @@
+﻿using AwesomeGIC.Domain.Entities;
+using AwesomeGIC.Domain.Interfaces;
+using AwesomeGIC.Domain.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Xunit;
+
+public class BankTransactionServiceTests
+{
+    private readonly Mock<IBankTransactionRepository> _mockRepository;
+    private readonly BankTransactionService _service;
+    private readonly Mock<IInterestRuleRepository> _mockInterestRuleRepository;
+    private readonly InterestRuleService _interestRuleService;
+
+    public BankTransactionServiceTests()
+    {
+        _mockInterestRuleRepository = new Mock<IInterestRuleRepository>();
+        _interestRuleService = new InterestRuleService(_mockInterestRuleRepository.Object);
+
+        _mockRepository = new Mock<IBankTransactionRepository>();
+        _service = new BankTransactionService(_mockRepository.Object, _interestRuleService);
+    }
+
+    [Fact]
+    public void GetTransactions_ShouldReturnAllTransactions()
+    {
+        // Arrange
+        var transactions = new List<BankTransaction> 
+        {
+            new BankTransaction
+            {
+                TxnId = "20230601-01",
+                AccountNumber = "AC001",
+                Amount = 100.0m,
+                Date = DateTime.Now,
+                Type = 'D',
+                Balance = 250
+            }
+        };
+
+        _mockRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<BankTransaction, bool>>>())).Returns(transactions);
+
+        // Act
+        var result = _service.GetBankTransactions("AC001");
+
+        // Assert
+        Assert.Equal(transactions, result);
+    }
+
+    [Fact]
+    public void AddTransaction_ShouldThrowException_WhenAccountNumberIsInvalid()
+    {
+        // Arrange
+        var transactions = new List<BankTransaction>
+        {
+            new BankTransaction
+            {
+                TxnId = "20230601-01",
+                AccountNumber = "AC001",
+                Amount = 100.0m,
+                Date = DateTime.Now,
+                Type = 'D',
+                Balance = 250
+            }
+        };
+
+        _mockRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<BankTransaction, bool>>>())).Returns(transactions);
+
+        var transaction = new BankTransaction
+        {
+            TxnId = "20230601-01",
+            AccountNumber = "",
+            Amount = 100.0m,
+            Date = DateTime.Now,
+            Type = 'D',
+            Balance = 250
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ApplicationException>(() => _service.AddBankTransaction(transaction));
+
+        Assert.Equal("Account number is required.", exception.Message);
+    }
+
+    [Fact]
+    public void AddTransaction_ShouldThrowException_WhenTypeIsInvalid()
+    {
+        // Arrange
+        var transactions = new List<BankTransaction>
+        {
+            new BankTransaction
+            {
+                TxnId = "20230601-01",
+                AccountNumber = "AC001",
+                Amount = 100.0m,
+                Date = DateTime.Now,
+                Type = 'D',
+                Balance = 250
+            }
+        };
+
+        _mockRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<BankTransaction, bool>>>())).Returns(transactions);
+
+        var transaction = new BankTransaction
+        {
+            TxnId = "20230601-01",
+            AccountNumber = "AC001",
+            Amount = 100.0m,
+            Date = DateTime.Now,
+            Type = 'X',
+            Balance = 250
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ApplicationException>(() => _service.AddBankTransaction(transaction));
+
+        Assert.Equal("Transaction type must be either 'D' or 'W'.", exception.Message);
+    }
+
+    [Fact]
+    public void AddTransaction_ShouldThrowException_WhenAmountIsInvalid()
+    {
+        // Arrange
+        var transactions = new List<BankTransaction>
+        {
+            new BankTransaction
+            {
+                TxnId = "20230601-01",
+                AccountNumber = "AC001",
+                Amount = 100.0m,
+                Date = DateTime.Now,
+                Type = 'D',
+                Balance = 250
+            }
+        };
+
+        _mockRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<BankTransaction, bool>>>())).Returns(transactions);
+
+        var transaction = new BankTransaction 
+        {
+            TxnId = "20230601-01",
+            AccountNumber = "AC001",
+            Amount = -100.0m,
+            Date = DateTime.Now,
+            Type = 'D',
+            Balance = 250
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ApplicationException>(() => _service.AddBankTransaction(transaction));
+
+        Assert.Equal("Amount must be greater than zero, decimals are allowed up to 2 decimal places.", exception.Message);
+    }
+
+    [Fact]
+    public void AddTransaction_ShouldThrowException_WhenBalanceIsInvalid()
+    {
+        // Arrange
+        var transactions = new List<BankTransaction>
+        {
+            new BankTransaction
+            {
+                TxnId = "20230601-01",
+                AccountNumber = "AC001",
+                Amount = 100.0m,
+                Date = DateTime.Now,
+                Type = 'D'
+            }
+        };
+
+        _mockRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<BankTransaction, bool>>>())).Returns(transactions);
+
+        var transaction = new BankTransaction
+        {
+            TxnId = "20230601-02",
+            AccountNumber = "AC001",
+            Amount = 100.0m,
+            Date = DateTime.Now,
+            Type = 'W'
+        };
+
+        // Act & Assert
+        var exception = Assert.Throws<ApplicationException>(() => _service.AddBankTransaction(transaction));
+
+        Assert.Equal("Account balance should not be less than zero.", exception.Message);
+    }
+}
---
+++ AwesomeGIC.Tests/InterestRuleServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using AwesomeGIC.Domain.Entities;
+using AwesomeGIC.Domain.Interfaces;
+using AwesomeGIC.Domain.Services;
+using Moq;
+using System.Linq.Expressions;
+
+public class InterestRuleServiceTests
+{
+    private readonly Mock<IInterestRuleRepository> _mockRepository;
+    private readonly InterestRuleService _service;
+
+    public InterestRuleServiceTests()
+    {
+        _mockRepository = new Mock<IInterestRuleRepository>();
+        _service = new InterestRuleService(_mockRepository.Object);
+    }
+
+    [Fact]
+    public void AddInterestRule_ShouldAddRule_WhenRuleIsValidAndNotExists()
+    {
+        // Arrange
+        var interestRule = new InterestRule { RuleId = "1", Date = DateTime.Now, Rate = 1.5m };
+        _mockRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<InterestRule, bool>>>())).Returns(new List<InterestRule>());
+
+        // Act
+        _service.AddInterestRule(interestRule);
+
+        // Assert
+        _mockRepository.Verify(repo => repo.Add(It.IsAny<InterestRule>()), Times.Once);
+    }
+
+    [Fact]
+    public void AddInterestRule_ShouldUpdateRule_WhenRuleExists()
+    {
+        // Arrange
+        var interestRule = new InterestRule { RuleId = "1", Date = DateTime.Now, Rate = 1.5m };
+        _mockRepository.Setup(repo => repo.Find(It.IsAny<Expression<Func<InterestRule, bool>>>())).Returns(new List<InterestRule> { interestRule });
+
+        // Act
+        _service.AddInterestRule(interestRule);
+
+        // Assert
+        _mockRepository.Verify(repo => repo.Update(It.IsAny<InterestRule>()), Times.Once);
+    }
+
+    [Fact]
+    public void GetInterestRules_ShouldReturnAllRules()
+    {
+        // Arrange
+        var interestRules = new List<InterestRule> { new InterestRule { RuleId = "1", Date = DateTime.Now, Rate = 1.5m } };
+        _mockRepository.Setup(repo => repo.GetAll()).Returns(interestRules);
+
+        // Act
+        var result = _service.GetInterestRules();
+
+        // Assert
+        Assert.Equal(interestRules, result);
+    }
+
+    [Fact]
+    public void AddInterestRule_ShouldThrowException_WhenRuleIdIsEmpty()
+    {
+        // Arrange
+        var interestRule = new InterestRule { RuleId = "", Date = DateTime.Now, Rate = 1.5m };
+
+        // Act & Assert
+        var exception = Assert.Throws<ApplicationException>(() => _service.AddInterestRule(interestRule));
+        Assert.Equal("Rule ID is required.", exception.Message);
+    }
+
+    [Fact]
+    public void AddInterestRule_ShouldThrowException_WhenRateIsInvalid()
+    {
+        // Arrange
+        var interestRule = new InterestRule { RuleId = "1", Date = DateTime.Now, Rate = 0 };
+
+        // Act & Assert
+        var exception = Assert.Throws<ApplicationException>(() => _service.AddInterestRule(interestRule));
+        Assert.Equal("Interest rate should be greater than 0 and less than 100.", exception.Message);
+    }
+}
